1845f259f97ee4e87e77fcd2bf016ecaa728ec9a
Taylor Bantle <taylor@liquidata.co>
2020-10-23 15:34:38 -0700

    Add list pagination util

--- cypress/integration/utils/sharedTests/pagination.ts
+++ cypress/integration/utils/sharedTests/pagination.ts
@@ -7,16 +7,18 @@ import {
 
 const beVisible = newShouldArgs("be.visible");
 
-export const testPaginationForTable = (
-  tableDataCy: string,
-  scrollContainerDataCy: string,
+const testPagination = (
+  descBefore: string,
   beforeLen: number,
-  afterLen = beforeLen + 10,
+  descAfter: string,
+  afterLen: number,
+  itemsDataCy: string,
+  scrollContainerDataCy: string,
 ) => [
   newExpectation(
-    `should have no more than ${beforeLen} rows in table`,
-    `${tableDataCy} tr`,
-    newShouldArgs("be.visible.and.have.length.of.at.most", 51),
+    descBefore,
+    itemsDataCy,
+    newShouldArgs("be.visible.and.have.length.of.at.most", beforeLen),
   ),
   newExpectationWithScrollTo(
     "should paginate on scroll",
@@ -25,11 +27,41 @@ export const testPaginationForTable = (
     newScrollToPosition("bottom", scrollContainerDataCy),
   ),
   newExpectation(
-    `should have at least ${afterLen} rows in table`,
-    `${tableDataCy} tr`,
+    descAfter,
+    itemsDataCy,
     newShouldArgs("be.visible.and.have.length.of.at.least", afterLen),
   ),
 ];
+
+export const testPaginationForTable = (
+  tableDataCy: string,
+  scrollContainerDataCy: string,
+  beforeLen: number,
+  afterLen = beforeLen + 10,
+) =>
+  testPagination(
+    `should have no more than ${beforeLen} rows in table`,
+    beforeLen,
+    `should have at least ${afterLen} rows in table`,
+    afterLen,
+    `${tableDataCy} tr`,
+    scrollContainerDataCy,
+  );
+
+export const testPaginationForList = (
+  listDataCy: string,
+  scrollContainerDataCy: string,
+  beforeLen: number,
+  afterLen = beforeLen + 10,
+) =>
+  testPagination(
+    `should have no more than ${beforeLen} items in list`,
+    beforeLen,
+    `should have at least ${afterLen} items in list`,
+    afterLen,
+    `${listDataCy} li`,
+    scrollContainerDataCy,
+  );
 
 export const testPaginationForRepoDataTable = testPaginationForTable(
   "[data-cy=repo-data-table]",
