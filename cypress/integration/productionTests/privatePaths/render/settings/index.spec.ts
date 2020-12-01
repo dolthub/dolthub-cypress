844dc5db7f247ecf0fbfd95ae9c5fbc9cc604d5e
Katie McCulloch <katie@dolthub.com>
2020-12-01 15:48:47 -0800

    cypress/integration/productionTests/privatePaths/render/settings/index.spec.ts: Add Settings test file for logged in user that tests for Settings header

---
+++ cypress/integration/productionTests/privatePaths/render/settings/index.spec.ts
@@ -0,0 +1,20 @@
+import { runTestsForDevices } from "../../../../utils";
+import { desktopDevicesForAppLayout } from "../../../../utils/devices";
+import { newExpectation, newShouldArgs } from "../../../../utils/helpers";
+
+const pageName = "Settings";
+const currentPage = "/settings";
+const loggedIn = true;
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  const tests = [
+    newExpectation(
+      "should render Settings header",
+      "[data-cy=settings-header]",
+      newShouldArgs("be.visible"),
+    ),
+  ];
+
+  const devices = desktopDevicesForAppLayout(pageName, tests, false, loggedIn);
+  runTestsForDevices({ currentPage, devices });
+});
