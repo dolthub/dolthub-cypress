525f0bdd91f1c4696f4dc14f1e8a5abd14b118a5
Katie McCulloch <katie@dolthub.com>
2020-12-02 16:41:55 -0800

    cypress/integration/productionTests/publicPaths/render/settings/index.spec.ts: Assert that a logged out user visiting /settings gets redirected to /signin page

--- cypress/integration/productionTests/publicPaths/render/settings/index.spec.ts
+++ cypress/integration/productionTests/publicPaths/render/settings/index.spec.ts
@@ -1,21 +1,17 @@
 import { runTestsForDevices } from "../../../../utils";
 import { macbook15ForAppLayout } from "../../../../utils/devices";
-import {
-  newExpectationWithRedirect,
-  newShouldArgs,
-} from "../../../../utils/helpers";
+import { newExpectation, newShouldArgs } from "../../../../utils/helpers";
 
 const pageName = "Settings page for logged out user";
 const currentPage = "/settings";
 
 describe(`${pageName} redirects to the /signin page for logged out users on different devices`, () => {
   const tests = [
-    newExpectationWithRedirect(
+    newExpectation(
       "should be redirected to signin page",
       "[data-cy=signup-why-join]",
       newShouldArgs("be.visible.and.contain", "Why join DoltHub?"),
       false,
-      "settings",
     ),
   ];
 
