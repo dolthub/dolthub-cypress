6c9501f9655b8460a97111929b524b1a588b0a01
Taylor Bantle <taylor@liquidata.co>
2021-01-21 11:52:20 -0800

    Add tests for webhooks pages

---
+++ cypress/integration/productionTests/publicPaths/render/webhooks/index.spec.ts
@@ -0,0 +1,32 @@
+import { runTestsForDevices } from "../../../../utils";
+import { macbook15ForAppLayout } from "../../../../utils/devices";
+import { newExpectation, newShouldArgs } from "../../../../utils/helpers";
+
+const pageName = "Webhooks page logged out";
+const currentOwner = "automated_testing";
+const currentRepo = "corona-virus";
+const currentPage = `repositories/${currentOwner}/${currentRepo}/webhooks`;
+const loggedIn = false;
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  const beVisible = newShouldArgs("be.visible");
+  const tests = [
+    newExpectation(
+      "should have table layout",
+      "[data-cy=table-layout-container]",
+      beVisible,
+    ),
+    newExpectation(
+      "should render 404 page",
+      "[data-cy=404-page]",
+      newShouldArgs(
+        "be.visible.and.contain",
+        "Must have repo write perms to manage webhooks",
+      ),
+    ),
+  ];
+
+  const devices = [macbook15ForAppLayout(pageName, tests, false, loggedIn)];
+
+  runTestsForDevices({ currentPage, devices });
+});
