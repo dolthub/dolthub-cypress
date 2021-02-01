ad8248f9b45efd69706302aa9b5126981309a2f0
Katie McCulloch <katie@dolthub.com>
2021-02-01 12:23:31 -0800

    cypress/integration/productionTests/publicPaths/render/issue: Add coronaClosedIssue and coronaOpenIssue public path tests

--- cypress/integration/productionTests/publicPaths/render/issue/coronaClosedIssue.spec.ts
+++ cypress/integration/productionTests/publicPaths/render/issue/coronaClosedIssue.spec.ts
@@ -1,3 +1,4 @@
+import { testIssuePageForAll } from "cypress/integration/utils/sharedTests/issuePage";
 import { testSignInTo } from "cypress/integration/utils/sharedTests/signInTo";
 import { runTestsForDevices } from "../../../../utils";
 import { macbook15ForAppLayout } from "../../../../utils/devices";
@@ -10,40 +11,20 @@ const currentIssueId = "8";
 const currentPage = `repositories/${currentOwner}/${currentRepo}/issues/${currentIssueId}`;
 
 describe(`${pageName} renders expected components on different devices`, () => {
-  const beVisible = newShouldArgs("be.visible");
   const notExist = newShouldArgs("not.exist");
 
   const tests = [
     newExpectation(
-      "should show title",
-      "[data-cy=issue-page-title]",
-      beVisible,
-    ),
-    newExpectation(
-      "should show description",
-      "[data-cy=issue-page-description]",
-      beVisible,
+      "should not find 404 page",
+      "[data-cy=issue-404-page]",
+      notExist,
     ),
     newExpectation(
       "should not show edit description button for logged out user",
       "[data-cy=issue-page-edit-description-button]",
       notExist,
     ),
-    newExpectation(
-      "should show closed state label",
-      "[data-cy=issue-state-label]",
-      newShouldArgs("be.visible.and.contain", "Closed"),
-    ),
-    newExpectation(
-      "should show details section",
-      "[data-cy=issue-page-details]",
-      beVisible,
-    ),
-    newExpectation(
-      "should not find 404 page",
-      "[data-cy=issue-404-page]",
-      notExist,
-    ),
+    ...testIssuePageForAll(currentOwner, currentRepo, currentIssueId, "Closed"),
     ...testSignInTo("comment on issues"),
   ];
 
--- cypress/integration/productionTests/publicPaths/render/issue/coronaOpenIssue.spec.ts
+++ cypress/integration/productionTests/publicPaths/render/issue/coronaOpenIssue.spec.ts
@@ -1,3 +1,4 @@
+import { testIssuePageForAll } from "cypress/integration/utils/sharedTests/issuePage";
 import { testSignInTo } from "cypress/integration/utils/sharedTests/signInTo";
 import { runTestsForDevices } from "../../../../utils";
 import { macbook15ForAppLayout } from "../../../../utils/devices";
@@ -10,7 +11,6 @@ const currentIssueId = "7";
 const currentPage = `repositories/${currentOwner}/${currentRepo}/issues/${currentIssueId}`;
 
 describe(`${pageName} renders expected components on different devices`, () => {
-  const beVisible = newShouldArgs("be.visible");
   const notExist = newShouldArgs("not.exist");
 
   const tests = [
@@ -20,30 +20,11 @@ describe(`${pageName} renders expected components on different devices`, () => {
       notExist,
     ),
     newExpectation(
-      "should show title",
-      "[data-cy=issue-page-title]",
-      beVisible,
-    ),
-    newExpectation(
-      "should show description",
-      "[data-cy=issue-page-description]",
-      beVisible,
-    ),
-    newExpectation(
       "should not show edit description button for logged out user",
       "[data-cy=issue-page-edit-description-button]",
       notExist,
     ),
-    newExpectation(
-      "should show open state label",
-      "[data-cy=issue-state-label]",
-      newShouldArgs("be.visible.and.contain", "Open"),
-    ),
-    newExpectation(
-      "should show details section",
-      "[data-cy=issue-page-details]",
-      beVisible,
-    ),
+    ...testIssuePageForAll(currentOwner, currentRepo, currentIssueId, "Open"),
     ...testSignInTo("comment on issues"),
   ];
 
