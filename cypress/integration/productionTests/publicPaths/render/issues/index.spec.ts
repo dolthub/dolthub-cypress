6ce649a5cb08449469cbbcfc943291f6fdfce3ad
Katie McCulloch <katie@dolthub.com>
2021-02-01 11:39:16 -0800

    Issues page testing

---
+++ cypress/integration/productionTests/publicPaths/render/issues/index.spec.ts
@@ -0,0 +1,65 @@
+import { runTestsForDevices } from "../../../../utils";
+import { macbook15ForAppLayout } from "../../../../utils/devices";
+import { newExpectation, newShouldArgs } from "../../../../utils/helpers";
+
+const pageName = "Issue page";
+const currentOwner = "automated_testing";
+const currentRepo = "corona-virus";
+const currentPage = `repositories/${currentOwner}/${currentRepo}/issues`;
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  const beVisible = newShouldArgs("be.visible");
+  const notExist = newShouldArgs("not.exist");
+
+  const tests = [
+    newExpectation(
+      "should find Issues header",
+      "[data-cy=repo-details-header]",
+      newShouldArgs("be.visible.and.contain", "Issues"),
+    ),
+    newExpectation(
+      "should find back to repo link",
+      "[data-cy=back-to-repo-link]",
+      newShouldArgs("be.visible.and.contain", `back to ${currentRepo}`),
+    ),
+    newExpectation(
+      "should find create issue button",
+      "[data-cy=new-issue-button]",
+      beVisible,
+    ),
+    newExpectation(
+      "should not find empty issue message",
+      "[data-cy=issue-no-issues]",
+      notExist,
+    ),
+    newExpectation(
+      "should find issue table with header",
+      "[data-cy=issue-table] > thead > tr > th",
+      newShouldArgs("be.visible.and.have.length", 5),
+    ),
+    newExpectation(
+      "should find at least 2 issues",
+      "[data-cy=issue-table] > tbody > tr",
+      newShouldArgs("be.visible.and.have.length.of.at.least", 2),
+    ),
+    newExpectation(
+      "should find issue with ID 8 with 5 columns",
+      "[data-cy=issue-row-8] > td",
+      newShouldArgs("be.visible.and.have.length", 5),
+    ),
+    newExpectation(
+      "should find issue with ID 8 with two links",
+      "[data-cy=issue-row-8] > td a",
+      newShouldArgs("be.visible.and.have.length", 2),
+    ),
+    newExpectation(
+      "should find issue with ID 8 with issue state label",
+      "[data-cy=issue-row-8] [data-cy=issue-state-label]",
+      beVisible,
+    ),
+  ];
+
+  const devices = [macbook15ForAppLayout(pageName, tests)];
+
+  runTestsForDevices({ currentPage, devices });
+});
