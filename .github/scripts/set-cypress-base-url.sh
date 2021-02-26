db906a7d8c1977d9e0ad382b875826a9e4f59fe8
Katie McCulloch <katie@dolthub.com>
2021-02-26 12:25:17 -0800

    .github: Fix output from cypress-base-url

--- .github/scripts/set-cypress-base-url.sh
+++ .github/scripts/set-cypress-base-url.sh
@@ -8,8 +8,6 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-echo "This is the service input to determine base_url: $1"
-
 if [ "$1" = "dolthub" ]; then
   echo "::set-output name=cypress_base_url::https://dolthub.awsdev.ld-corp.com"
 elif [ "$1" = "dolthub-v3" ]; then
