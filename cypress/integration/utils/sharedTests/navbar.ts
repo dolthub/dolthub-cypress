8c1be1d36a9d20567f97af2cdd15af23638f0972
Katie McCulloch <katie@dolthub.com>
2021-05-15 10:12:05 -0700

    cypress/integration/utils/sharedTests/navbar.ts: Update data-cy

--- cypress/integration/utils/sharedTests/navbar.ts
+++ cypress/integration/utils/sharedTests/navbar.ts
@@ -21,7 +21,7 @@ const sharedLinks = [
 const signedOutNavbarLinks = [
   ...sharedLinks,
   "[data-cy=navbar-discover]",
-  "[data-cy=navbar-signin]",
+  "[data-cy=navbar-desktop-signin-button]",
 ];
 
 const signedInNavbarLinks = [...sharedLinks, "[data-cy=navbar-repositories]"];
