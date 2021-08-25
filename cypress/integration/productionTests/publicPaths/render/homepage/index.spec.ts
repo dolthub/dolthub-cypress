8d89c7b904bc5f8e77539866e465d0000d510f0f
Taylor Bantle <taylor@liquidata.co>
2021-08-25 11:33:50 -0700

    Add basic homepage test

--- cypress/integration/productionTests/publicPaths/render/homepage/index.spec.ts
+++ cypress/integration/productionTests/publicPaths/render/homepage/index.spec.ts
@@ -1,148 +1,26 @@
 import { runTestsForDevices } from "../../../../utils";
-import { allDevicesDiffTestsForSignedOut } from "../../../../utils/devices";
-import {
-  newExpectation,
-  newExpectationWithClickFlows,
-  newExpectationWithScrollIntoView,
-  newShouldArgs,
-  scrollToPosition,
-} from "../../../../utils/helpers";
-import { testMobileMailingList } from "../../../../utils/sharedTests/mailingList";
-import {
-  checkRepoListForTab,
-  mostRecentReposClickFlow,
-  testMobileRepoList,
-} from "../../../../utils/sharedTests/reposContainer";
-import {
-  testBlogArticles,
-  testDoltReleaseLink,
-} from "../../../../utils/sharedTests/sidecar";
-import { Expectation } from "../../../../utils/types";
+import { allDevicesForSignedOut } from "../../../../utils/devices";
+import { newExpectation, newShouldArgs } from "../../../../utils/helpers";
 
 const pageName = "Homepage";
 const currentPage = "/";
 
 describe(`${pageName} renders expected components on different devices`, () => {
-  const beVisible = newShouldArgs("be.visible");
-  const exist = newShouldArgs("exist");
-
-  const scrollToPositionInContainer = (
-    position: Cypress.PositionType,
-  ): Expectation => scrollToPosition("#scroll-container", position);
-
-  const testHero = [
+  const tests = [
     newExpectation(
-      "should have hero with correct titles",
-      "[data-cy=home-page-hero-desktop]",
+      "renders expected headers",
+      "#main-content",
       newShouldArgs("be.visible.and.contain", [
-        "Welcome to DoltHub",
-        "Version Controlled Database",
-        "Collaboration Platform",
+        "The database evolved",
+        "A next generation database",
+        "Two familiar interfaces combined",
+        "Evolved Features",
       ]),
     ),
   ];
 
-  const testMobileHero = [
-    newExpectation(
-      "should have mobile hero with correct titles",
-      "[data-cy=home-page-hero-mobile]",
-      newShouldArgs("be.visible.and.contain", [
-        "Welcome to DoltHub",
-        "Version Controlled Database",
-        "Collaboration Platform",
-      ]),
-    ),
-    newExpectation(
-      "should have list of details in hero on mobile",
-      "[data-cy=hero-mobile-list]",
-      beVisible,
-    ),
-  ];
+  const devices = allDevicesForSignedOut(pageName, tests, tests);
 
-  const testReposContainer = [
-    newExpectationWithScrollIntoView(
-      "should have repos container",
-      "[data-cy=repos-container-with-tabs]",
-      newShouldArgs("be.visible.and.contain", [
-        "Featured",
-        "Most recent",
-        "Most starred",
-      ]),
-      true,
-    ),
-    ...checkRepoListForTab("featured", 5),
-    newExpectationWithClickFlows(
-      "should have list of most-recent repos",
-      "[data-cy=most-recent-repos-tab]",
-      beVisible,
-      [mostRecentReposClickFlow],
-    ),
-    ...checkRepoListForTab("most-starred", 20),
-  ];
-
-  const testSearchInput = newExpectation(
-    "should have repository search bar",
-    "[data-cy=search-input-signed-out]",
-    beVisible,
-  );
-
-  const testSidecarsInView = [
-    testDoltReleaseLink(beVisible),
-    newExpectation(
-      "should have an about section",
-      "[data-cy=home-page-about] > p",
-      newShouldArgs("exist.and.have.length", 3),
-    ),
-    testBlogArticles(exist),
-  ];
-
-  const testSidecarsOutOfView = [
-    testDoltReleaseLink(exist),
-    newExpectation(
-      "should have an about section",
-      "[data-cy=home-page-about] > p",
-      newShouldArgs("exist.and.have.length", 3),
-    ),
-    testBlogArticles(exist),
-  ];
-
-  const desktopTests = [
-    ...testHero,
-    testSearchInput,
-    ...testSidecarsInView,
-    ...testReposContainer,
-  ];
-
-  const iPadTests = [
-    newExpectation(
-      "should have create account button on iPad",
-      "[data-cy=hero-mobile-create-account-button]",
-      beVisible,
-    ),
-    ...testMobileHero,
-    testSearchInput,
-    scrollToPositionInContainer("center"),
-    ...testReposContainer,
-    ...testSidecarsOutOfView,
-  ];
-
-  const iPhoneTests = [
-    scrollToPositionInContainer("top"),
-    ...testMobileHero,
-    testBlogArticles(exist),
-    scrollToPositionInContainer("center"),
-    ...testMobileRepoList("[data-cy=homepage-repo-lists]"),
-    ...testMobileMailingList("[data-cy=homepage-mobile-mailing-list]"),
-  ];
-
-  const devices = allDevicesDiffTestsForSignedOut(
-    pageName,
-    desktopTests,
-    iPadTests,
-    iPhoneTests,
-  );
-
-  // TODO: unskip for new homepage
-  const skip = true;
+  const skip = false;
   runTestsForDevices({ currentPage, devices, skip });
 });
