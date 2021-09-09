96d19baa0e98caf46be450cec8677c347cb28369
Taylor Bantle <taylor@liquidata.co>
2021-09-09 13:33:36 -0700

    Add tests for dolthub and blog paths that don't exist

--- cypress/integration/productionTests/publicPaths/render/blog/article.spec.ts
+++ cypress/integration/productionTests/publicPaths/render/blog/article.spec.ts
@@ -13,41 +13,31 @@ describe(`${pageName} renders expected components on different devices`, () => {
   const beVisible = newShouldArgs("be.visible");
 
   const testBlogArticle = [
-    newExpectation(
-      "should have blog post",
-      "[data-cy=blog-post]",
-      beVisible,
-      skip,
-    ),
+    newExpectation("should have blog post", "[data-cy=blog-post]", beVisible),
     newExpectation(
       "should have blog home link at top and bottom",
       "[data-cy=blog-home-back-link]",
       newShouldArgs("exist.and.have.length", 2),
-      skip,
     ),
     newExpectation(
       "should have header with title",
       "[data-cy=blog-post] > header h1",
       beVisible,
-      skip,
     ),
     newExpectation(
       "should have header with time",
       "[data-cy=blog-post] > header time",
       beVisible,
-      skip,
     ),
     newExpectation(
       "should have blog post text",
       "[data-cy=blog-post-text]",
       beVisible,
-      skip,
     ),
     newExpectation(
       "should have share widget",
       "[data-cy=blog-share-widget]",
       beVisible,
-      skip,
     ),
   ];
 
@@ -56,5 +46,5 @@ describe(`${pageName} renders expected components on different devices`, () => {
     testBlogArticle,
     testBlogArticle,
   );
-  runTestsForDevices({ currentPage, devices });
+  runTestsForDevices({ currentPage, devices, skip });
 });
