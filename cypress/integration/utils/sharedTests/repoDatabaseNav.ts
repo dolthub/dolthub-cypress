f532a51d2604f67e4c02367197e1896ca2bc4a1f
Kathleen F Young <kathleenfyoung@Kathleens-MacBook-Pro.local>
2021-11-15 17:36:00 -0800

    Fixed it

--- cypress/integration/utils/sharedTests/repoDatabaseNav.ts
+++ cypress/integration/utils/sharedTests/repoDatabaseNav.ts
@@ -10,65 +10,64 @@ import { ClickFlow, Expectation, Tests } from "../types";
 const beVisible = newShouldArgs("be.visible");
 const notExist = newShouldArgs("not.exist");
 
-// [
-//   expectation for active tables tab,
-//   click flow for views with expectations for active views tab/inactive tables tab,
-//   click flow for queries tab with expectations for active queries tab/inactive views tab,
-//   click flow for schemas tab with expectations for active schemas tab/inactive queries tab
-// ]
-
 export const clickToOpenNavClickFlow: ClickFlow = newClickFlow(
   "[data-cy=left-nav-toggle-icon]",
   [
-    newExpectationWithClickFlows(
+    newExpectation(
       "the Tables tab should be active",
       `[data-cy=active-tab-tables]`,
       beVisible,
-      [
-        newClickFlow(`[data-cy=tab-views]`, [
-          newExpectation(
-            "Tables should now be inactive",
-            `[data-cy=tab-tables]`,
-            beVisible,
-          ),
-        ]),
-      ],
     ),
-    newExpectationWithClickFlows(
+  ],
+);
+
+export const checkViewsClickflow: ClickFlow = newClickFlow(
+  `[data-cy=tab-views]`,
+  [
+    newExpectation(
+      "the Tables tab should be inactive",
+      `[data-cy=tab-tables]`,
+      beVisible,
+    ),
+    newExpectation(
       "the Views tab should be active",
       `[data-cy=active-tab-views]`,
       beVisible,
-      [
-        newClickFlow(`[data-cy=tab-queries]`, [
-          newExpectation(
-            "Views should now be inactive",
-            `[data-cy=tab-views]`,
-            beVisible,
-          ),
-        ]),
-      ],
     ),
-    newExpectationWithClickFlows(
+  ],
+);
+
+export const checkQueriesClickflow: ClickFlow = newClickFlow(
+  `[data-cy=tab-queries]`,
+  [
+    newExpectation(
+      "the Views tab should be inactive",
+      `[data-cy=tab-views]`,
+      beVisible,
+    ),
+    newExpectation(
       "the Queries tab should be active",
       `[data-cy=active-tab-queries]`,
       beVisible,
-      [
-        newClickFlow(`[data-cy=tab-schemas]`, [
-          newExpectation(
-            "Queries should now be inactive",
-            `[data-cy=tab-queries]`,
-            beVisible,
-          ),
-        ]),
-      ],
-    ),
-    newExpectationWithClickFlows(
-      "the Schema tab should be active",
-      `[data-cy=active-tab-schemas]`,
-      beVisible,
-      [newClickFlow(`[data-cy=tab-tables]`, [])],
     ),
   ],
+);
+
+export const checkSchemaClickflow: ClickFlow = newClickFlow(
+  `[data-cy=tab-schema]`,
+  [
+    newExpectation(
+      "the Queries tab should be inactive",
+      `[data-cy=tab-queries]`,
+      beVisible,
+    ),
+    newExpectation(
+      "the Schemas tab should be active",
+      `[data-cy=active-tab-schemas]`,
+      beVisible,
+    ),
+  ],
+  `[data-cy=tab-tables]`,
 );
 
 // TABLES
@@ -138,7 +137,7 @@ export const testTablesSection = (
       "should open left database navigation",
       "[data-cy=left-nav-toggle-icon]",
       beVisible,
-      [clickToOpenNavClickFlow],
+      [clickToOpenNavClickFlow, checkViewsClickflow, checkQueriesClickflow, checkSchemaClickflow],
     ),
     ...tableExpectations(tableLen, testTable),
   ];
