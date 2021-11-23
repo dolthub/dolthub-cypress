c211db4ea7066bbaf0bd36d49923e3787ca77e8f
Kathleen F Young <kathleenfyoung@Kathleens-MacBook-Pro.local>
2021-11-23 14:12:37 -0800

    ran prettier

--- cypress/integration/productionTests/privatePaths/render/databaseReadmeTests/index.spec.ts
+++ cypress/integration/productionTests/privatePaths/render/databaseReadmeTests/index.spec.ts
@@ -2,7 +2,7 @@ import { runTestsForDevices } from "../../../../utils";
 import { macbook15ForAppLayout } from "../../../../utils/devices";
 import {
   newClickFlow,
-    newExpectation,
+  newExpectation,
   newShouldArgs,
   newExpectationWithClickFlows,
   //   newExpectationWithScrollIntoView,
@@ -35,33 +35,31 @@ describe(`${pageName} renders expected components on different devices`, () => {
       ],
     ),
     newExpectationWithTypeString(
-        "should write description in textbox",
-        "[data-cy=textarea-container]",
-        beVisible,
-        "test"
+      "should write description in textbox",
+      "[data-cy=textarea-container]",
+      beVisible,
+      "test",
     ),
     newExpectationWithClickFlows(
-        "should create the new doc",
-        "[data-cy=new-doc-create-button]",
-        beVisible,
-        [
-          newClickFlow(
-            "[data-cy=new-doc-create-button]",
-            [],
-          ),
-        ],
-      ),
+      "should create the new doc",
+      "[data-cy=new-doc-create-button]",
+      beVisible,
+      [newClickFlow("[data-cy=new-doc-create-button]", [])],
+    ),
     newExpectationWithClickFlows(
-        "should merge doc",
-        "[data-cy=merge-button]",
-        beVisible,
-        [
-          newClickFlow(
+      "should merge doc",
+      "[data-cy=merge-button]",
+      beVisible,
+      [
+        newClickFlow("[data-cy=merge-button]", [
+          newExpectation(
+            "Should say 'merging'",
             "[data-cy=merge-button]",
-            [newExpectation("Should say 'merging'", "[data-cy=merge-button]", beVisibleAndContain("Merging..."))]
+            beVisibleAndContain("Merging..."),
           ),
-        ],
-      ),
+        ]),
+      ],
+    ),
     //! Gotta figure out how to make branches delete
     // newExpectationWithClickFlows(
     //     "should delete branch",
@@ -75,45 +73,49 @@ describe(`${pageName} renders expected components on different devices`, () => {
     //     ],
     //   ),
     newExpectationWithClickFlows(
-        "the new license should render in the abut tab",
-        "[data-cy=repo-about-tab]",
-        beVisible,
-        [
-          newClickFlow(
-            "[data-cy=repo-about-tab]",
-            [newExpectation("should contain 'test'", "[data-cy=repo-doc-markdown]", beVisibleAndContain('test'))],
-            // "[data-cy=edit-docs-button]",
-          ),
-        ],
-      ),
-      newExpectationWithTypeString(
-        "should write a new description in textbox",
-        "[data-cy=textarea-container]",
-        beVisible,
-        "test number 2"
+      "the new license should render in the abut tab",
+      "[data-cy=repo-about-tab]",
+      beVisible,
+      [
+        newClickFlow(
+          "[data-cy=repo-about-tab]",
+          [
+            newExpectation(
+              "should contain 'test'",
+              "[data-cy=repo-doc-markdown]",
+              beVisibleAndContain("test"),
+            ),
+          ],
+          // "[data-cy=edit-docs-button]",
+        ),
+      ],
+    ),
+    newExpectationWithTypeString(
+      "should write a new description in textbox",
+      "[data-cy=textarea-container]",
+      beVisible,
+      "test number 2",
     ),
     newExpectationWithClickFlows(
-        "should submit the edited doc",
-        "[data-cy=submit-edit-docs-button]",
-        beVisible,
-        [
-          newClickFlow(
-            "[data-cy=submit-edit-docs-button]",
-            [],
-          ),
-        ],
-      ),
-      newExpectationWithClickFlows(
-        "should merge edited doc",
-        "[data-cy=merge-button]",
-        beVisible,
-        [
-          newClickFlow(
+      "should submit the edited doc",
+      "[data-cy=submit-edit-docs-button]",
+      beVisible,
+      [newClickFlow("[data-cy=submit-edit-docs-button]", [])],
+    ),
+    newExpectationWithClickFlows(
+      "should merge edited doc",
+      "[data-cy=merge-button]",
+      beVisible,
+      [
+        newClickFlow("[data-cy=merge-button]", [
+          newExpectation(
+            "Should say 'merging'",
             "[data-cy=merge-button]",
-            [newExpectation("Should say 'merging'", "[data-cy=merge-button]", beVisibleAndContain("Merging..."))]
+            beVisibleAndContain("Merging..."),
           ),
-        ],
-      ),
+        ]),
+      ],
+    ),
     //! Gotta figure out how to make branches delete
     // newExpectationWithClickFlows(
     //     "should delete branch",
@@ -127,39 +129,43 @@ describe(`${pageName} renders expected components on different devices`, () => {
     //     ],
     //   ),
     newExpectationWithClickFlows(
-        "the edited license should render in the abut tab",
-        "[data-cy=repo-about-tab]",
-        beVisible,
-        [
-          newClickFlow(
-            "[data-cy=repo-about-tab]",
-            [newExpectation("should contain 'test'", "[data-cy=repo-doc-markdown]", beVisibleAndContain('test number 2'))],
-            "[data-cy=delete-docs-button]",
-          ),
-        ],
-      ),
+      "the edited license should render in the abut tab",
+      "[data-cy=repo-about-tab]",
+      beVisible,
+      [
+        newClickFlow(
+          "[data-cy=repo-about-tab]",
+          [
+            newExpectation(
+              "should contain 'test'",
+              "[data-cy=repo-doc-markdown]",
+              beVisibleAndContain("test number 2"),
+            ),
+          ],
+          "[data-cy=delete-docs-button]",
+        ),
+      ],
+    ),
     newExpectationWithClickFlows(
-        "should be able to delete the docs",
-        "[data-cy=confirm-delete-docs-button]",
-        beVisible,
-        [
-          newClickFlow(
-            "[data-cy=confirm-delete-docs-button]",
-            [],
-          ),
-        ],
-      ),
-      newExpectationWithClickFlows(
-        "should merge deleted doc",
-        "[data-cy=merge-button]",
-        beVisible,
-        [
-          newClickFlow(
+      "should be able to delete the docs",
+      "[data-cy=confirm-delete-docs-button]",
+      beVisible,
+      [newClickFlow("[data-cy=confirm-delete-docs-button]", [])],
+    ),
+    newExpectationWithClickFlows(
+      "should merge deleted doc",
+      "[data-cy=merge-button]",
+      beVisible,
+      [
+        newClickFlow("[data-cy=merge-button]", [
+          newExpectation(
+            "Should say 'merging'",
             "[data-cy=merge-button]",
-            [newExpectation("Should say 'merging'", "[data-cy=merge-button]", beVisibleAndContain("Merging..."))]
+            beVisibleAndContain("Merging..."),
           ),
-        ],
-      ),
+        ]),
+      ],
+    ),
     //! Gotta figure out how to make branches delete
     // newExpectationWithClickFlows(
     //     "should delete branch",
