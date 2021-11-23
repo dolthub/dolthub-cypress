adac1e4757cd707b580f3e0bbb59c23df912753a
Kathleen F Young <kathleenfyoung@Kathleens-MacBook-Pro.local>
2021-11-23 11:46:41 -0800

    Almost done writing tests

--- cypress/integration/productionTests/privatePaths/render/databaseReadmeTests/index.spec.ts
+++ cypress/integration/productionTests/privatePaths/render/databaseReadmeTests/index.spec.ts
@@ -2,43 +2,183 @@ import { runTestsForDevices } from "../../../../utils";
 import { macbook15ForAppLayout } from "../../../../utils/devices";
 import {
   newClickFlow,
-//   newExpectation,
+  newExpectation,
   newShouldArgs,
   newExpectationWithClickFlows,
-//   newExpectationWithScrollIntoView,
+  //   newExpectationWithScrollIntoView,
+  newExpectationWithTypeString,
 } from "../../../../utils/helpers";
 
 const pageName = "Creating, Editing, and Deleting a readme";
 const currentOwner = "automated_testing";
 const currentRepo = "readme_testing_repo";
-const currentPage = `repositories/${currentOwner}/${currentRepo}/settings`;
+const currentPage = `repositories/${currentOwner}/${currentRepo}`;
 const loggedIn = true;
 
 const beVisible = newShouldArgs("be.visible");
 // const notExist = newShouldArgs("not.exist");
-// const beVisibleAndContain = (value: string) =>
-//   newShouldArgs("be.visible.and.contain", value);
-
+const beVisibleAndContain = (value: string) =>
+  newShouldArgs("be.visible.and.contain", value);
 
 describe(`${pageName} renders expected components on different devices`, () => {
   const tests = [
     newExpectationWithClickFlows(
-        "should have functioning nav dropdown",
-        "[data-cy=dropdown-database-nav]",
-        beVisible,
-        [newClickFlow("[data-cy=dropdown-database-nav]", [], "[data-cy=dropdown-database-nav]"),]
-    )
-    // newExpectation(
-    //   "should have Settings header",
-    //   "[data-cy=repo-settings-header]",
-    //   beVisible,
+      "should navigate to the new readme page",
+      "[data-cy=dropdown-database-nav]",
+      beVisible,
+      [
+        newClickFlow(
+          "[data-cy=dropdown-database-nav]",
+          [],
+          "[data-cy=dropdown-new-readme-link]",
+        ),
+      ],
+    ),
+    newExpectationWithTypeString(
+      "should write description in textbox",
+      "[data-cy=textarea-container]",
+      beVisible,
+      "test",
+    ),
+    newExpectationWithClickFlows(
+      "should create the new doc",
+      "[data-cy=new-doc-create-button]",
+      beVisible,
+      [newClickFlow("[data-cy=new-doc-create-button]", [])],
+    ),
+    newExpectationWithClickFlows(
+      "should merge doc",
+      "[data-cy=merge-button]",
+      beVisible,
+      [
+        newClickFlow("[data-cy=merge-button]", [
+          newExpectation(
+            "Should say 'merging'",
+            "[data-cy=merge-button]",
+            beVisibleAndContain("Merging..."),
+          ),
+        ]),
+      ],
+    ),
+    //! Gotta figure out how to make branches delete
+    // newExpectationWithClickFlows(
+    //     "should delete branch",
+    //     "[data=cy=delete-branch-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data=cy=delete-branch-button]",
+    //         []
+    //       ),
+    //     ],
+    //   ),
+    newExpectationWithClickFlows(
+      "the new license should render in the abut tab",
+      "[data-cy=repo-about-tab]",
+      beVisible,
+      [
+        newClickFlow(
+          "[data-cy=repo-about-tab]",
+          [
+            newExpectation(
+              "should contain 'test'",
+              "[data-cy=repo-doc-markdown]",
+              beVisibleAndContain("test"),
+            ),
+          ],
+          // "[data-cy=edit-docs-button]",
+        ),
+      ],
+    ),
+    //   newExpectationWithTypeString(
+    //     "should write a new description in textbox",
+    //     "[data-cy=textarea-container]",
+    //     beVisible,
+    //     "test number 2"
     // ),
     // newExpectationWithClickFlows(
-    //   "should have a Delete Database button that opens a modal",
-    //   "[data-cy=delete-database-button]",
-    //   beVisible,
-    //   [deleteDatabaseModalClickflow],
-    // ),
+    //     "should submit the edited doc",
+    //     "[data-cy=submit-doc-edit-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data-cy=submit-doc-edit-button]",
+    //         [],
+    //       ),
+    //     ],
+    //   ),
+    //   newExpectationWithClickFlows(
+    //     "should merge edited doc",
+    //     "[data-cy=merge-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data-cy=merge-button]",
+    //         [newExpectation("Should say 'merging'", "[data-cy=merge-button]", beVisibleAndContain("Merging..."))]
+    //       ),
+    //     ],
+    //   ),
+    // //! Gotta figure out how to make branches delete
+    // // newExpectationWithClickFlows(
+    // //     "should delete branch",
+    // //     "[data=cy=delete-branch-button]",
+    // //     beVisible,
+    // //     [
+    // //       newClickFlow(
+    // //         "[data=cy=delete-branch-button]",
+    // //         []
+    // //       ),
+    // //     ],
+    // //   ),
+    // newExpectationWithClickFlows(
+    //     "the edited license should render in the abut tab",
+    //     "[data-cy=repo-about-tab]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data-cy=repo-about-tab]",
+    //         [
+    //             // expect edited doc to be there
+    //             // expect it to show the edits
+    //         ],
+    //         "[data-cy=delete-docs-button]",
+    //       ),
+    //     ],
+    //   ),
+    // newExpectationWithClickFlows(
+    //     "should be able to delete the docs",
+    //     "[data-cy=confirm-delete-docs-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data-cy=confirm-delete-docs-button]",
+    //         [],
+    //       ),
+    //     ],
+    //   ),
+    //   newExpectationWithClickFlows(
+    //     "should merge deleted doc",
+    //     "[data-cy=merge-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data-cy=merge-button]",
+    //         [newExpectation("Should say 'merging'", "[data-cy=merge-button]", beVisibleAndContain("Merging..."))]
+    //       ),
+    //     ],
+    //   ),
+    //! Gotta figure out how to make branches delete
+    // newExpectationWithClickFlows(
+    //     "should delete branch",
+    //     "[data=cy=delete-branch-button]",
+    //     beVisible,
+    //     [
+    //       newClickFlow(
+    //         "[data=cy=delete-branch-button]",
+    //         []
+    //       ),
+    //     ],
+    //   ),
   ];
   const skip = false;
   const devices = [macbook15ForAppLayout(pageName, tests, false, loggedIn)];
