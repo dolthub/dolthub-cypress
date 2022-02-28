27b7b7fd770d40f55bd18bf5a6d1cdc162213e85
liuliu <liuliu.developer@gmail.com>
2022-02-28 11:38:22 -0800

    test discover page mobile selector

--- cypress/integration/dolthub/productionTests/publicPaths/render/discover/index.spec.ts
+++ cypress/integration/dolthub/productionTests/publicPaths/render/discover/index.spec.ts
@@ -1,5 +1,9 @@
 import { runTestsForDevices } from "../../../../../utils";
-import { allDevicesForSignedOut } from "../../../../../utils/devices";
+import {
+  iPad2ForAppLayout,
+  iPhoneXForAppLayout,
+  macbook15ForAppLayout,
+} from "../../../../../utils/devices";
 import {
   newExpectation,
   newExpectationWithClickFlows,
@@ -8,6 +12,7 @@ import {
 import {
   checkRepoListForTab,
   mostRecentReposClickFlow,
+  mostRecentReposClickFlowMobile,
 } from "../../../../../utils/sharedTests/reposContainer";
 
 const pageName = "Discover page";
@@ -35,8 +40,30 @@ describe(`${pageName} renders expected components on different devices`, () => {
       [mostRecentReposClickFlow],
     ),
   ];
-
-  const devices = allDevicesForSignedOut(pageName, tests, tests);
+  const mobileTests = [
+    newExpectation(
+      "should have repository search input",
+      "[data-cy=search-input]",
+      beVisible,
+    ),
+    newExpectation(
+      "should have repos container",
+      "[data-cy=repos-container-with-tabs]",
+      newShouldArgs("be.visible.and.contain", ["Featured", "Discover"]),
+    ),
+    ...checkRepoListForTab("featured", 5),
+    newExpectationWithClickFlows(
+      "should have list of most-recent repos",
+      "[data-cy=discover-mobile-selector]",
+      beVisible,
+      [mostRecentReposClickFlowMobile],
+    ),
+  ];
+  const devices = [
+    macbook15ForAppLayout(pageName, tests),
+    iPad2ForAppLayout(pageName, tests),
+    iPhoneXForAppLayout(pageName, mobileTests),
+  ];
 
   runTestsForDevices({ currentPage, devices });
 });
