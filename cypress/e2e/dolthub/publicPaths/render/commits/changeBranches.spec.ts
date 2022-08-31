14eb3f5357db89f9003e24b170b2ada0b1e3b935
Mario Lopez <mariodolt@Marios-MacBook-Pro.local>
2022-08-31 14:42:02 -0700

    missed the  comment

--- cypress/e2e/dolthub/publicPaths/render/commits/changeBranches.spec.ts
+++ cypress/e2e/dolthub/publicPaths/render/commits/changeBranches.spec.ts
@@ -6,20 +6,23 @@ const pageName = "Commit log page";
 const currentOwner = "automated_testing";
 const currentRepo = "repo_with_branch_protection";
 const currentBranch = "branch_can_not_be_deleted";
-const currentPath = `repositories/${currentOwner}/${currentRepo}/commits/`;
-const currentPage = `${currentPath}${currentBranch}`;
+const currentPage = `repositories/${currentOwner}/${currentRepo}/commits/`;
 
 describe(`${pageName} renders expected components on different devices`, () => {
   const changeBranchParams = {
     isLeftNavClosed: true,
     currentTabDataCy: "commit-log-commits-list",
     destinationBranch: "main",
-    destinationURL: `/${currentPath}main`,
+    destinationURL: `/${currentPage}main`,
   };
 
   const devices = [
     macbook15ForAppLayout(pageName, changeBranch(changeBranchParams)),
   ];
   const skip = false;
-  runTestsForDevices({ currentPage, devices, skip });
+  runTestsForDevices({
+    currentPage: `${currentPage}${currentBranch}`,
+    devices,
+    skip,
+  });
 });
