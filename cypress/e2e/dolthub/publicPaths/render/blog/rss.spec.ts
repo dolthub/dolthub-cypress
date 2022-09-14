96f3b6dfc3c25b26fe71d4f2e77ce4d5028433a6
Mario Lopez <mariodolt@Marios-MacBook-Pro.local>
2022-09-14 16:43:13 -0700

    prettier fix?

--- cypress/e2e/dolthub/publicPaths/render/blog/rss.spec.ts
+++ cypress/e2e/dolthub/publicPaths/render/blog/rss.spec.ts
@@ -1,14 +1,12 @@
-const pageName = "Blog RSS feed page";
-const currentPage = "/blog/rss.xml";
 const skip = !!Cypress.env("LOCAL_DOLTHUB") || !!Cypress.env("LOCAL_BLOG");
 
-describe(`${pageName} renders expected components on different devices`, () => {
+describe(`Blog RSS feed page renders expected components on different devices`, () => {
   if (skip) {
     xit("gets blog RSS feed", () => {});
   } else {
     it("gets blog RSS feed", () => {
-      cy.request({ url: currentPage }).its("status").should("equal", 200);
-      cy.request({ url: currentPage })
+      cy.request({ url: "/blog/rss.xml" }).its("status").should("equal", 200);
+      cy.request({ url: "/blog/rss.xml" })
         .its("body")
         .should("contain", "DoltHub Blog");
     });
