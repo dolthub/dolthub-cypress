28942ec060a1fddcbd2099225affda8c28b6671f
Mario Lopez <mariodolt@marios-mbp.lan>
2022-09-13 22:43:22 -0700

    basic outline

--- cypress/e2e/dolthub/publicPaths/render/solutions/data-ingestion.spec.ts
+++ cypress/e2e/dolthub/publicPaths/render/solutions/data-ingestion.spec.ts
@@ -4,89 +4,31 @@ import {
   findAndBeVisible,
   findAndContains,
 } from "cypress/e2e/utils/sharedTests/sharedFunctionsAndVariables";
+import {
+  topRightFindAndContain,
+  topBottomFindAndContain,
+  bulletQueryFindAndContain,
+  bulletScriptFindAndContain,
+  bulletVersionFindAndContain,
+  doltInActionTopFindAndContain,
+  doltInActionListFindAndContain,
+  exampleItemsFindAndContain,
+} from "../../../../utils/constants/data-IngestionConstants";
 import { runTestsForDevices } from "../../../../utils";
-import { allDevicesForSignedOut } from "../../../../utils/devices";
-import { newExpectationWithScrollIntoView } from "../../../../utils/helpers";
+import {
+  desktopDevicesForSignedOut,
+  iPad2ForAppLayout,
+} from "../../../../utils/devices";
+import {
+  newClickFlow,
+  newExpectationWithClickFlows,
+  newExpectationWithScrollIntoView,
+} from "../../../../utils/helpers";
 
 const pageName = "Data-ingestion page";
 const currentPage = "/solutions/data-ingestion";
 
 describe(`${pageName} renders expected components on different devices`, () => {
-  const topRightFindAndContain = [
-    { dataCy: "top-right-title", text: "DATA IS CRITICAL" },
-    { dataCy: "top-right-header", text: "Do you trust your data sources?" },
-    { dataCy: "top-right-list-item-one", text: "How do you know when" },
-    {
-      dataCy: "top-right-list-item-two",
-      text: "How fast can you recover from bad data?",
-    },
-    {
-      dataCy: "top-right-list-item-three",
-      text: "How quickly can you identify it?",
-    },
-  ];
-
-  const topBottomFindAndContain = [
-    { dataCy: "top-bottom-title", text: "REMOVING THE RISK" },
-    { dataCy: "top-bottom-header", text: "Dolt versions data like code" },
-    { dataCy: "top-bottom-list-item-one", text: "Cell level version control" },
-    {
-      dataCy: "top-bottom-list-item-two",
-      text: "Human and machine readable data diffs",
-    },
-    {
-      dataCy: "top-bottom-list-item-three",
-      text: "Instantaneous rollback and rollforward of data versions",
-    },
-    {
-      dataCy: "top-bottom-list-item-four",
-      text: "A pull request workflow for ad-hoc updates, complete with CI/CD",
-    },
-  ];
-
-  const bulletQueryFindAndContain = [
-    { dataCy: "bullet-query-header", text: "Query" },
-    { dataCy: "bullet-query-item-one", text: "Full SQL interface" },
-    { dataCy: "bullet-query-item-two", text: "Query any version from SQL" },
-    { dataCy: "bullet-query-item-three", text: "Identify cell level changes" },
-  ];
-
-  const bulletScriptFindAndContain = [
-    { dataCy: "bullet-script-header", text: "Script" },
-    {
-      dataCy: "bullet-script-item-one",
-      text: "Test incoming data with scriptable business logic",
-    },
-    {
-      dataCy: "bullet-script-item-two",
-      text: "Run validations against only deltas for efficient testing",
-    },
-  ];
-
-  const bulletVersionFindAndContain = [
-    { dataCy: "bullet-version-header", text: "Version" },
-    {
-      dataCy: "bullet-version-item-one",
-      text: "Track a cell value through time",
-    },
-    {
-      dataCy: "bullet-version-item-two",
-      text: "Script logic for time series analyses",
-    },
-    {
-      dataCy: "bullet-version-item-three",
-      text: "Rollback and rollforward instantly",
-    },
-  ];
-
-  const doltInActionTopFindAndContain = [
-    { dataCy: "doltInAction-title", text: "Dolt In Action" },
-    {
-      dataCy: "doltInAction-description",
-      text: "In this demo we use Dolt and GitHub Actions to version a public dataset and run SQL against the result",
-    },
-  ];
-
   const tests = [
     findAndBeVisible("top-right-container"),
     ...topRightFindAndContain.map(find =>
@@ -153,7 +95,7 @@ describe(`${pageName} renders expected components on different devices`, () => {
     ),
 
     newExpectationWithScrollIntoView(
-      "should find Dolt In Action top container",
+      "should find Dolt In Action: top container",
       "[data-cy=doltInAction-top-container]",
       beVisible,
       true,
@@ -161,9 +103,41 @@ describe(`${pageName} renders expected components on different devices`, () => {
     ...doltInActionTopFindAndContain.map(find =>
       findAndContains(find.dataCy, find.text),
     ),
+
+    newExpectationWithScrollIntoView(
+      "should find Dolt In Action: List",
+      "[data-cy=doltInAction-list]",
+      beVisible,
+      true,
+    ),
+    ...doltInActionListFindAndContain.map(find =>
+      findAndContains(find.dataCy, find.text),
+    ),
+
+    newExpectationWithScrollIntoView(
+      "should find workflow item",
+      "[data-cy=workflow-item]",
+      beVisible,
+      true,
+    ),
+    ...exampleItemsFindAndContain.map(
+      find => (
+        findAndContains(find.dataCyTitle, find.textTitle),
+        findAndContains(find.dataCyDescription, find.textDescription),
+        newExpectationWithClickFlows(
+          "should find right button",
+          "[aria-label=example-right-button]",
+          beVisible,
+          [newClickFlow("[aria-label=example-right-button]", [])],
+        )
+      ),
+    ),
   ];
 
-  const devices = allDevicesForSignedOut(pageName, tests, tests);
+  const devices = [
+    ...desktopDevicesForSignedOut(pageName, tests, false),
+    iPad2ForAppLayout(pageName, tests),
+  ];
 
   runTestsForDevices({ currentPage, devices });
 });
---
+++ cypress/e2e/utils/constants/data-IngestionConstants.ts
@@ -0,0 +1,115 @@
+export const topRightFindAndContain = [
+  { dataCy: "top-right-title", text: "DATA IS CRITICAL" },
+  { dataCy: "top-right-header", text: "Do you trust your data sources?" },
+  { dataCy: "top-right-list-item-one", text: "How do you know when" },
+  {
+    dataCy: "top-right-list-item-two",
+    text: "How fast can you recover from bad data?",
+  },
+  {
+    dataCy: "top-right-list-item-three",
+    text: "How quickly can you identify it?",
+  },
+];
+
+export const topBottomFindAndContain = [
+  { dataCy: "top-bottom-title", text: "REMOVING THE RISK" },
+  { dataCy: "top-bottom-header", text: "Dolt versions data like code" },
+  { dataCy: "top-bottom-list-item-one", text: "Cell level version control" },
+  {
+    dataCy: "top-bottom-list-item-two",
+    text: "Human and machine readable data diffs",
+  },
+  {
+    dataCy: "top-bottom-list-item-three",
+    text: "Instantaneous rollback and rollforward of data versions",
+  },
+  {
+    dataCy: "top-bottom-list-item-four",
+    text: "A pull request workflow for ad-hoc updates, complete with CI/CD",
+  },
+];
+
+export const bulletQueryFindAndContain = [
+  { dataCy: "bullet-query-header", text: "Query" },
+  { dataCy: "bullet-query-item-one", text: "Full SQL interface" },
+  { dataCy: "bullet-query-item-two", text: "Query any version from SQL" },
+  { dataCy: "bullet-query-item-three", text: "Identify cell level changes" },
+];
+
+export const bulletScriptFindAndContain = [
+  { dataCy: "bullet-script-header", text: "Script" },
+  {
+    dataCy: "bullet-script-item-one",
+    text: "Test incoming data with scriptable business logic",
+  },
+  {
+    dataCy: "bullet-script-item-two",
+    text: "Run validations against only deltas for efficient testing",
+  },
+];
+
+export const bulletVersionFindAndContain = [
+  { dataCy: "bullet-version-header", text: "Version" },
+  {
+    dataCy: "bullet-version-item-one",
+    text: "Track a cell value through time",
+  },
+  {
+    dataCy: "bullet-version-item-two",
+    text: "Script logic for time series analyses",
+  },
+  {
+    dataCy: "bullet-version-item-three",
+    text: "Rollback and rollforward instantly",
+  },
+];
+
+export const doltInActionTopFindAndContain = [
+  { dataCy: "doltInAction-title", text: "Dolt In Action" },
+  {
+    dataCy: "doltInAction-description",
+    text: "In this demo we use Dolt and GitHub Actions to version a public dataset and run SQL against the result",
+  },
+];
+
+export const doltInActionListFindAndContain = [
+  {
+    dataCy: "doltInAction-list-item-one",
+    text: "Simple, fully customizable GitHub Action driven ETL process",
+  },
+  {
+    dataCy: "doltInAction-list-item-two",
+    text: "Run SQL over every version of the data",
+  },
+  {
+    dataCy: "doltInAction-list-item-three",
+    text: "Data diffs in SQL tables",
+  },
+  {
+    dataCy: "doltInAction-list-item-four",
+    text: "A commit for each version of the data",
+  },
+];
+
+export const exampleItemsFindAndContain = [
+  {
+    dataCyTitle: "workflow-title",
+    textTitle: "The workflow file",
+    dataCyDescription: "workflow-description",
+    textDescription:
+      "A simple GitHub Action definition that pushes to a Dolt database hosted on DoltHub",
+  },
+  {
+    dataCyTitle: "script-title",
+    textTitle: "The script",
+    dataCyDescription: "script-description",
+    textDescription: "Doltpy, our Python API, turns data into Dolt tables",
+  },
+  {
+    dataCyTitle: "output-title",
+    textTitle: "The output",
+    dataCyDescription: "output-description",
+    textDescription: "See it in action, running daily",
+  },
+];
