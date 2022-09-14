46251c5967c3e6e2bda6c006c046fb77c765cdda
Mario Lopez <mariodolt@Marios-MacBook-Pro.local>
2022-09-14 15:33:54 -0700

    basic test to check is sitemap exists

---
+++ cypress/e2e/dolthub/publicPaths/render/sitemap/index.spex.ts
@@ -0,0 +1,9 @@
+const pageName = "sitemap page";
+const currentPage = "/sitemap.xml";
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  it("gets site map", () => {
+    cy.request({ url: currentPage }).its("status").should("equal", 200);
+    cy.request({ url: currentPage }).its("body").should("contain", "url");
+  });
+});
