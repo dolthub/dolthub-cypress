10e8b0441031b1edb471409f5e178e92d58b41c5
Mario Lopez <mariodolt@Marios-MacBook-Pro.local>
2022-11-28 10:23:05 -0800

    first pass at tests

--- cypress/e2e/dolthub/publicPaths/render/pricing/index.spec.ts
+++ cypress/e2e/dolthub/publicPaths/render/pricing/index.spec.ts
@@ -1,22 +1,109 @@
 import { allDevicesForSignedOut } from "@utils/devices";
-import { newExpectation, newShouldArgs } from "@utils/helpers";
+import { newExpectationWithScrollIntoView } from "@utils/helpers";
 import { runTestsForDevices } from "@utils/index";
+import {
+  beVisible,
+  shouldFindAndContain,
+} from "@utils/sharedTests/sharedFunctionsAndVariables";
 
 const pageName = "Pricing page";
 const currentPage = "/pricing";
 
+const pricingTests = [
+  {
+    card: "dolt-card",
+    shouldFind: [
+      {
+        datacy: "dolt-header",
+        text: "GIT FOR DATA",
+      },
+      {
+        datacy: "dolt-link",
+        text: "Download for free",
+      },
+    ],
+  },
+  {
+    card: "hosted-dolt-card",
+    shouldFind: [
+      {
+        datacy: "hosted-dolt-header",
+        text: "DOLT IN THE CLOUD",
+      },
+      {
+        datacy: "hosted-dolt-get-started-button",
+        text: "Get Started",
+      },
+      {
+        datacy: "hosted-dolt-pricing-link",
+        text: "See detailed pricing",
+      },
+    ],
+  },
+  {
+    card: "dolthub-card",
+    shouldFind: [
+      {
+        datacy: "dolthub-header",
+        text: "FORKS, CLONES, & PULLS REQUESTS",
+      },
+      {
+        datacy: "dolthub-sign-up-button",
+        text: "Sign up",
+      },
+      {
+        datacy: "dolthub-pro-button",
+        text: "Get DoltHub Pro",
+      },
+    ],
+  },
+  {
+    card: "dolt-lab-card",
+    shouldFind: [
+      {
+        datacy: "dolt-lab-header",
+        text: "SELF-HOSTED DOLTHUB",
+      },
+      {
+        datacy: "dolt-lab-download-button",
+        text: "Download for free",
+      },
+      {
+        datacy: "dolt-lab-contact-link",
+        text: "Contact Enterprise Sales ",
+      },
+    ],
+  },
+  {
+    card: "enterprise-card",
+    shouldFind: [
+      {
+        datacy: "enterprise-header",
+        text: "Enterprise Support",
+      },
+      {
+        datacy: "enterprise-contact-button",
+        text: "Contact Sales",
+      },
+    ],
+  },
+];
+
 describe(`${pageName} renders expected components on different devices`, () => {
   const tests = [
-    newExpectation(
-      "should render pricing info",
-      "[data-cy=pricing-info]",
-      newShouldArgs("be.visible.and.contain", [
-        "Dolt Pricing",
-        "Community",
-        "Business",
-        "Enterprise",
-      ]),
-    ),
+    ...pricingTests
+      .map(test => [
+        newExpectationWithScrollIntoView(
+          "should have Push existing database section",
+          `[data-cy=${test.card}]`,
+          beVisible,
+          true,
+        ),
+        ...test.shouldFind.map(find =>
+          shouldFindAndContain(find.datacy, find.text),
+        ),
+      ])
+      .flat(),
   ];
 
   const devices = allDevicesForSignedOut(pageName, tests, tests);
