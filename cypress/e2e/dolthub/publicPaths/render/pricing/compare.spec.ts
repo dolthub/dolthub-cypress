33aec6c233f3448c0bb652f847e91290af70cc8b
Mario Lopez <mariodolt@marios-mbp.lan>
2022-12-02 07:37:50 -0800

    add tests

---
+++ cypress/e2e/dolthub/publicPaths/render/pricing/compare.spec.ts
@@ -0,0 +1,51 @@
+import {
+  allDevicesForSignedOut,
+  desktopDevicesForSignedOut,
+  iPhoneX,
+  mobileDevicesForSignedOut,
+} from "@utils/devices";
+import { newExpectationWithScrollIntoView } from "@utils/helpers";
+import { runTestsForDevices } from "@utils/index";
+import {
+  beVisible,
+  shouldBeVisible,
+} from "@utils/sharedTests/sharedFunctionsAndVariables";
+
+const pageName = "Compare page";
+const currentPage = "/compare";
+
+const pricingTests = [
+  "dolt-label",
+  "hosted-dolt-starting",
+  "dolthub-plans",
+  "doltlab-label",
+];
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  const desktopTests = [
+    shouldBeVisible("desktop-table-container"),
+    ...pricingTests.map(datacy => shouldBeVisible(datacy)),
+  ];
+
+  const mobileTests = [
+    shouldBeVisible("mobile-table-container"),
+    ...pricingTests.map(datacy =>
+      newExpectationWithScrollIntoView(
+        `should find and scroll to ${datacy} table cell`,
+        `[data-cy=mobile-${datacy}]`,
+        beVisible,
+        true,
+      ),
+    ),
+  ];
+
+  const desktopDevices = desktopDevicesForSignedOut(pageName, desktopTests);
+  const iphone = iPhoneX(pageName, mobileTests, false);
+
+  const skip = false;
+  runTestsForDevices({
+    currentPage,
+    devices: [...desktopDevices, iphone],
+    skip,
+  });
+});
---
+++ cypress/e2e/dolthub/publicPaths/render/pricing/switchPages.spec.ts
@@ -0,0 +1,67 @@
+import {
+  allDevicesForSignedOut,
+  desktopDevicesForSignedOut,
+  iPhoneX,
+  mobileDevicesForSignedOut,
+} from "@utils/devices";
+import {
+  newClickFlow,
+  newExpectation,
+  newExpectationWithClickFlows,
+  newExpectationWithScrollIntoView,
+} from "@utils/helpers";
+import { runTestsForDevices } from "@utils/index";
+import {
+  beVisible,
+  shouldBeVisible,
+} from "@utils/sharedTests/sharedFunctionsAndVariables";
+import { Expectation } from "@utils/types";
+
+const pageName = "Compare page";
+const currentPage = "/compare";
+
+type SwitchTest = {
+  routeTo: string;
+  find: string;
+};
+
+function getSwitchTests(isMobile: boolean): SwitchTest[] {
+  const switchTests = [
+    { routeTo: "to-compare", find: "table-container" },
+    { routeTo: "to-pricing", find: "dolt-card" },
+  ];
+  if (isMobile) {
+    switchTests[0].find = `mobile-${switchTests[0].find}`;
+  }
+  return switchTests;
+}
+
+describe(`${pageName} renders expected components on different devices`, () => {
+  function getTests(isMobile: boolean): Expectation[] {
+    return getSwitchTests(isMobile)
+      .map(test => [
+        newExpectationWithClickFlows(
+          "should show and select the branch",
+          `[data-cy=${test.routeTo}]`,
+          beVisible,
+          [
+            newClickFlow(`[data-cy=${test.routeTo}]`, [
+              shouldBeVisible(test.routeTo),
+            ]),
+          ],
+        ),
+        shouldBeVisible(test.routeTo),
+      ])
+      .flat();
+  }
+
+  const desktopDevices = desktopDevicesForSignedOut(pageName, getTests(false));
+  const iphone = iPhoneX(pageName, getTests(true), false);
+
+  const skip = false;
+  runTestsForDevices({
+    currentPage,
+    devices: [...desktopDevices, iphone],
+    skip,
+  });
+});
