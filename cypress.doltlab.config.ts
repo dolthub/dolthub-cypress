05cabc04f50de5e09a20a2dda6bc6110f157f093
LiuLiu <liuliu.developer@gmail.com>
2023-01-09 16:38:18 -0800

    doltlab config

--- cypress.doltlab.config.ts
+++ cypress.doltlab.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://doltlab.dolthub.com",
     specPattern: "cypress/e2e/doltlab/**/*.{js,jsx,ts,tsx}",
+    testIsolation: false,
   },
   viewportWidth: 1440,
   viewportHeight: 900,
