28ec018c0fe0580fc8d2b3e38960c2368c46d3ea
Taylor Bantle <taylor@dolthub.com>
2023-05-02 11:41:21 -0700

    Fix org/user profile tests

--- cypress/e2e/dolthub/publicPaths/render/organizations/noRepos.spec.ts
+++ cypress/e2e/dolthub/publicPaths/render/organizations/noRepos.spec.ts
@@ -1,130 +1,65 @@
-import { allDevicesForAppLayout } from "@utils/devices";
-import { newExpectation, newShouldArgs } from "@utils/helpers";
+import {
+  shouldBeVisible,
+  shouldFindAndContain,
+  shouldNotExist,
+} from "@sharedTests/sharedFunctionsAndVariables";
+import { iPhoneXForAppLayout, macbook15ForAppLayout } from "@utils/devices";
 import { runTestsForDevices } from "@utils/index";
 
 const pageName = "Organization page with no repos";
 const orgName = "no-repos-org";
 const currentPage = `/organizations/${orgName}`;
 
+const commonDataCy = [
+  "profile-card",
+  "profile-card-pic",
+  "profile-card-bio",
+  "no-repos-msg",
+];
+
+const commonNotExist = [
+  "org-member-list",
+  "org-settings",
+  "billing-info",
+  "payment-history",
+  "repository-list-for-owner",
+];
+
+const desktopDataCy = ["profile-summary", "org-profile-databases-tab"];
+
+const desktopNotExist = [
+  "org-profile-people-tab",
+  "org-profile-settings-tab",
+  "org-profile-billing-tab",
+  "org-profile-payment-history-tab",
+  "profile-card-url",
+  "profile-card-location",
+  "add-location-link",
+  "add-website-link",
+];
+
 describe(`${pageName} renders expected components on different devices`, () => {
-  const beVisible = newShouldArgs("be.visible");
-  const notBeVisible = newShouldArgs("not.be.visible");
-  const notExist = newShouldArgs("not.exist");
-
-  const commonTest = [
-    newExpectation(
-      "should show profile card",
-      "[data-cy=profile-card]",
-      beVisible,
-    ),
-    newExpectation(
-      "should show profile card profile pic",
-      "[data-cy=profile-card-pic]",
-      beVisible,
-    ),
-    newExpectation(
-      "should show profile summary",
-      "[data-cy=profile-summary]",
-      beVisible,
-    ),
-    newExpectation(
-      "should not show next steps",
-      "[data-cy=profile-card-next-steps]",
-      notExist,
-    ),
-  ];
-
-  const testProfileCardDesktop = [
-    newExpectation(
-      "should show profile card profile name",
-      "[data-cy=profile-card-name-desktop]",
-      newShouldArgs("be.visible.and.contain", orgName),
-    ),
-  ];
-
-  const testProfileCardMobile = [
-    newExpectation(
-      "should show profile card profile name",
-      "[data-cy=profile-card-name-mobile]",
-      newShouldArgs("be.visible.and.contain", orgName),
-    ),
-  ];
-
-  const testTabContainer = [
-    newExpectation(
-      "should show tabs container",
-      "[data-cy=org-profile-tabs-container]",
-      beVisible,
-    ),
-    newExpectation(
-      "should show Databases tab",
-      "[data-cy=org-profile-databases-tab]",
-      beVisible,
-    ),
-    newExpectation(
-      "should have disabled People tab",
-      "[data-cy=org-profile-people-tab]",
-      notBeVisible,
-    ),
-    newExpectation(
-      "should have disabled Settings tab",
-      "[data-cy=org-profile-settings-tab]",
-      notBeVisible,
-    ),
-    newExpectation(
-      "should have disabled Billing tab",
-      "[data-cy=org-profile-billing-tab]",
-      notBeVisible,
-    ),
-    newExpectation(
-      "should have disabled Payment History tab",
-      "[data-cy=org-profile-payment-history-tab]",
-      notBeVisible,
-    ),
-    newExpectation(
-      "should show no repos message",
-      "[data-cy=no-repos-msg]",
-      beVisible,
-    ),
-    newExpectation(
-      "should not show list of owner repositories",
-      "[data-cy=repository-list-for-owner]",
-      notExist,
-    ),
-    newExpectation(
-      "should not show org member list",
-      "[data-cy=org-member-list]",
-      notExist,
-    ),
-    newExpectation(
-      "should not show org settings",
-      "[data-cy=org-settings]",
-      notExist,
-    ),
-    newExpectation(
-      "should not show org billing info",
-      "[data-cy=billing-info]",
-      notExist,
-    ),
-    newExpectation(
-      "should not show org payment history",
-      "[data-cy=payment-history]",
-      notExist,
-    ),
+  const commonTests = [
+    ...commonDataCy.map(dc => shouldBeVisible(dc)),
+    shouldFindAndContain("profile-card-name", orgName),
+    ...commonNotExist.map(dc => shouldNotExist(dc)),
   ];
 
   const desktopTests = [
-    ...commonTest,
-    ...testProfileCardDesktop,
-    ...testTabContainer,
-  ];
-  const mobileTests = [
-    ...commonTest,
-    ...testProfileCardMobile,
-    ...testTabContainer,
+    ...commonTests,
+    ...desktopDataCy.map(dc => shouldBeVisible(dc)),
+    ...desktopNotExist.map(dc => shouldNotExist(dc)),
   ];
 
-  const devices = allDevicesForAppLayout(pageName, desktopTests, mobileTests);
+  const mobileTests = [
+    ...commonTests,
+    shouldFindAndContain("mobile-profile-selector", "Databases"),
+  ];
+
+  const devices = [
+    macbook15ForAppLayout(pageName, desktopTests, false),
+    iPhoneXForAppLayout(pageName, mobileTests),
+  ];
 
   runTestsForDevices({ currentPage, devices });
 });
