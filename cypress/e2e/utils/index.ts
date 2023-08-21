69c8c865d58ff4ce48c9cd5d73d8733d67579443
LiuLiu <liuliu.developer@gmail.com>
2023-08-21 11:58:13 -0700

    should be able to assert has.attr

--- cypress/e2e/utils/index.ts
+++ cypress/e2e/utils/index.ts
@@ -226,19 +226,18 @@ function getAssertionTest(
     scrollSelectorIntoView(selectorStr);
   }
   if (Array.isArray(shouldArgs.value)) {
-    if (shouldArgs.chainer !== "be.visible.and.contain") {
-      throw new Error(
-        "Value array can only be used with be.visible.and.contain",
-      );
-    }
-    return cy
+    if(shouldArgs.chainer === "be.visible.and.contain"){
+      return cy
       .get(selectorStr, opts)
       .should("be.visible")
       .should($el => {
-        shouldArgs.value.forEach((v: string) => {
-          expect($el).to.contain(v, message);
-        });
-      });
+        shouldArgs.value.forEach((v: string) => expect($el).to.contain(v, message))
+      });      
+    }
+      return cy
+      .get(selectorStr, opts)
+      .should(shouldArgs.chainer, ...shouldArgs.value, { message });
+    
   }
   return cy
     .get(selectorStr, opts)
