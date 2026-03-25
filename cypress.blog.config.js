284964b4f93c90adf885244ef9bfea57286cf87c
Taylor Bantle <taylor@dolthub.com>
2026-03-25 16:50:09 -0700

    Move blog to its own workflow since it's a separate app

---
+++ cypress.blog.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  projectId: "f6ssqp",
+
+  e2e: {
+    baseUrl: "https://www.dolthub.com",
+    specPattern: "cypress/e2e/blog/**/*.{js,jsx,ts,tsx}",
+    setupNodeEvents(on) {
+      on("task", {
+        log(message) {
+          console.log(message);
+
+          return null;
+        },
+      });
+    },
+  },
+
+  viewportWidth: 1440,
+  viewportHeight: 900,
+});
